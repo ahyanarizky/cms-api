0046c1cb0d8878ccc35488026f55f5ca7389fedf
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-11-17 14:38:09 +0700

    auth page 90%

--- client/src/javascripts/javascript.js
+++ client/src/javascripts/javascript.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-    $('#nav-logout').hide()
-    formRegister()
+    authPage()
 })
+
+// ---------------------------------------------------------------------------
+// AUTHENTICATION
+// ---------------------------------------------------------------------------
+
+const Auth = {
+    authenticateUser: (data) => {
+        if (data.status === 'error') console.log('No account:', data)
+        Auth.deauthenticateUser()
+            // console.log('data:', data)
+        localStorage.setItem('token', data.token)
+            // console.log('token:', localStorage.getItem('token'))
+    },
+    isUserAuthenticated: () => {
+        // console.log('token:', localStorage.getItem('token'))
+        return localStorage.getItem('token') !== null
+    },
+    deauthenticateUser: () => {
+        localStorage.removeItem('token')
+    },
+    getToken: () => {
+        return localStorage.getItem('token')
+    },
+    getUser: () => {
+        let token = Auth.getToken()
+        if (!token) return {}
+        else {
+            return jwt_decode(token)
+        }
+    }
+}
+
 
 function formRegister() {
     $('#main-container').empty()
@@ -17,18 +48,22 @@ function formRegister() {
   </li>
 </ul>
 <h1 class="text-center">Register</h1>
-<form id="form-login">
+<form id="form-register">
   <div class="form-group">
       <label for="email">Email</label>
-      <input type="email" class="form-control" id="input-email" placeholder="Enter Email" name='email'>
+      <input type="email" class="form-control" id="input-email-register" placeholder="Enter Email" name='email'>
   </div>
   <div class="form-group">
       <label for="username">Username</label>
-      <input type="text" class="form-control" id="input-username" placeholder="Enter Username" name='username'>
+      <input type="text" class="form-control" id="input-username-register" placeholder="Enter Username" name='username'>
   </div>
   <div class="form-group">
       <label for="password">Password</label>
-      <input type="password" class="form-control" id="input-password" placeholder="Password" name='password'>
+      <input type="password" class="form-control" id="input-password-register" placeholder="Password" name='password'>
+  </div>
+  <div class="form-group">
+      <label for="confirm-password">Confirm Password</label>
+      <input type="password" class="form-control" id="input-confirm-password-register" placeholder="Confirm Password" name='confirm'>
   </div>
   <div class="form-group">
       <button type="button" class="btn btn-primary" role="button" onclick="registerProcessClient()">Register</button>
@@ -38,6 +73,63 @@ function formRegister() {
 `
     $('#main-container').prepend(html)
 
+}
+
+function registerProcessClient() {
+    $('#alert-confirm').remove()
+    let $email = $('#input-email-register').val()
+    let $username = $('#input-username-register').val()
+    let $password = $('#input-password-register').val()
+    let $confirm = $('#input-confirm-password-register').val()
+    if ($password != $confirm) {
+        let html = ''
+        html += `
+          <div class="alert alert-danger" role="alert" id='alert-confirm'>
+            <strong>Confirm Password Not Match</strong> Please Try Again
+          </div>
+        `
+        $('#form-register').append(html)
+    } else {
+        $.ajax({
+            url: "http://localhost:3000/api/user/register",
+            method: "post",
+            data: {
+                email: $email,
+                username: $username,
+                password: $password
+            },
+            success: function(data) {
+                Auth.authenticateUser(data)
+                console.log(Auth.getToken());
+                $('#main-container').empty()
+                let html = ''
+                html += `
+              <div class="jumbotron">
+              <h1>Welcome ${Auth.getUser().username}</h1>
+              </div>
+
+              `
+                $('#main-container').append(html)
+                $('#nav-logout').show()
+                $('#nav-logout').on('click', processLogout)
+
+                let homepanel = `
+                <li id='nav-home'><a href="#">Home</a></li>
+                <li id="nav-create"><a href="#">Data</a></li>
+                <li id="nav-register"><a href="#">Data Date</a></li>
+                `
+                $('#navbar-ul-left').append(homepanel)
+            },
+            error: function(err) {
+                console.log('failed');
+                formRegister()
+                $('#nav-home').hide()
+                $('#nav-create').hide()
+                $('#nav-search').hide()
+                $('#nav-logout').hide()
+            }
+        })
+    }
 }
 
 function formLogin() {
@@ -57,15 +149,11 @@ function formLogin() {
 <form id="form-login">
     <div class="form-group">
         <label for="username">Email</label>
-        <input type="text" class="form-control" id="input-username" placeholder="Enter Username" name='username'>
+        <input type="text" class="form-control" id="input-username-login" placeholder="Enter Username" name='username'>
     </div>
     <div class="form-group">
         <label for="password">Password</label>
-        <input type="password" class="form-control" id="input-password" placeholder="Password" name='password'>
-    </div>
-    <div class="form-group">
-        <label for="confirm-password">Confirm Password</label>
-        <input type="password" class="form-control" id="input-confirm-password" placeholder="Confirm Password" name='confirm'>
+        <input type="password" class="form-control" id="input-password-login" placeholder="Password" name='password'>
     </div>
     <div class="form-group">
         <button type="button" class="btn btn-primary" role="button" onclick="loginProcessClient()">Login</button>
@@ -74,5 +162,67 @@ function formLogin() {
 </div>
 `
     $('#main-container').prepend(html)
+
+}
+
+function loginProcessClient() {
+    let $email = $('#input-email-login').val()
+    let $password = $('#input-password-login').val()
+
+    $.ajax({
+        url: "http://localhost:3000/api/user/login",
+        method: 'post',
+        data: {
+            email: $email,
+            password: $password
+        },
+        success: function(data) {
+            $('#main-container').empty()
+            Auth.authenticateUser(data)
+            console.log(Auth.getUser());
+            let html = ''
+            html += `
+          <div class="jumbotron">
+          <h1>Welcome ${Auth.getUser().username}</h1>
+          </div>
+
+          `
+            $('#main-container').append(html)
+            $('#nav-login').hide()
+            $('#nav-register').hide()
+            $('#nav-logout').show()
+            $('#nav-logout').on('click', processLogout)
+            let homepanel = `
+            <li id='nav-home'><a href="#">Home</a></li>
+            <li id="nav-create"><a href="#">Data</a></li>
+            <li id="nav-register"><a href="#">Data Date</a></li>
+            `
+            $('#navbar-ul-left').append(homepanel)
+
+        },
+        error: function(err) {
+            formRegister()
+            $('#nav-register').on('click', formRegister)
+            $('#nav-login').on('click', formLogin)
+            $('#nav-home').hide()
+            $('#nav-create').hide()
+            $('#nav-search').hide()
+            $('#nav-logout').hide()
+        }
+    })
+
+}
+
+function processLogout() {
+    localStorage.removeItem('token')
+    authPage()
+}
+
+function authPage() {
+    $('#nav-logout').hide()
+    formRegister()
+}
+
+function loadData() {
 
 }
